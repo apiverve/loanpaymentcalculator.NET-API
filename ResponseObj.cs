643a8f9d3c7ac43aca4d14c5d09152a0160b908d
APIVerve Team <hello@apiverve.com>
2024-05-14 15:29:24 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,60 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class amortizationschedule
+{
+    [JsonProperty("month")]
+    public int month { get; set; }
+
+    [JsonProperty("interest_payment")]
+    public double interestpayment { get; set; }
+
+    [JsonProperty("principal_payment")]
+    public double principalpayment { get; set; }
+
+    [JsonProperty("remaining_balance")]
+    public double remainingbalance { get; set; }
+
+}
+
+public class data
+{
+    [JsonProperty("loanAmount")]
+    public int loanAmount { get; set; }
+
+    [JsonProperty("downpayment")]
+    public int downpayment { get; set; }
+
+    [JsonProperty("interestRate")]
+    public double interestRate { get; set; }
+
+    [JsonProperty("loanTerm")]
+    public int loanTerm { get; set; }
+
+    [JsonProperty("monthly_payment")]
+    public double monthlypayment { get; set; }
+
+    [JsonProperty("total_interest_paid")]
+    public double totalinterestpaid { get; set; }
+
+    [JsonProperty("amortization_schedule")]
+    public amortizationschedule[] amortizationschedule { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
