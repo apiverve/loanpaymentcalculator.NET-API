ba414dda6ecf1fd097b468ceab9fcda1b93d4e98
APIVerve Team <hello@apiverve.com>
2024-08-20 10:57:44 -0500

    Bug Fixes

---
+++ LoanCalculatorQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class LoanCalculatorQueryOptions {
+public double loanAmount { get; set; }
+public double interestRate { get; set; }
+public int loanTerm { get; set; }
+}
+
+}
